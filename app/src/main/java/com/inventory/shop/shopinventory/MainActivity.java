4b266b01f22054c445fbf65fc9fd23f67a826507
devAjayAj <ajayajkumar18@gmail.com>
2017-05-22 22:15:38 +0530

    first commit

--- app/src/main/java/com/inventory/shop/shopinventory/MainActivity.java
+++ app/src/main/java/com/inventory/shop/shopinventory/MainActivity.java
@@ -5,18 +5,54 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         DatabaseHelper dbHelper = new DatabaseHelper(getApplicationContext());
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-        textv = (TextView)findViewById(R.id.tv);
-        textv.setText("All");
-     }
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
+        initializeData();
+        initializeAdapter();
+    }
+
+    public List<CategoryClass> categoryClassList;
+    RecyclerView recyclerView;
+
+    public void initializeData() {
+        categoryClassList = new ArrayList<>();
+        categoryClassList.add(new CategoryClass("1"));
+        categoryClassList.add(new CategoryClass("2"));
+        categoryClassList.add(new CategoryClass("3"));
+        categoryClassList.add(new CategoryClass("4"));
+        categoryClassList.add(new CategoryClass("5"));
+        categoryClassList.add(new CategoryClass("1"));
+        categoryClassList.add(new CategoryClass("2"));
+        categoryClassList.add(new CategoryClass("3"));
+        categoryClassList.add(new CategoryClass("4"));
+        categoryClassList.add(new CategoryClass("5"));
+        categoryClassList.add(new CategoryClass("1"));
+        categoryClassList.add(new CategoryClass("2"));
+        categoryClassList.add(new CategoryClass("3"));
+        categoryClassList.add(new CategoryClass("4"));
+        categoryClassList.add(new CategoryClass("5"));
+    }
+
+    public void initializeAdapter() {
+        RecyclerVIewAdapter adapter = new RecyclerVIewAdapter(categoryClassList);
+        recyclerView.setAdapter(adapter);
+    }
 }
--- app/src/main/java/com/inventory/shop/shopinventory/RecyclerVIewAdapter.java
+++ app/src/main/java/com/inventory/shop/shopinventory/RecyclerVIewAdapter.java
@@ -1,10 +1,13 @@
 package com.inventory.shop.shopinventory;
 
+import android.content.DialogInterface;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -13,20 +16,24 @@ import java.util.List;
  * Created by aj on 22/5/17.
  */
 
-public class RecyclerVIewAdapter extends RecyclerView.Adapter<RecyclerVIewAdapter.CategoryClassViewHolder>{
+public class RecyclerVIewAdapter extends RecyclerView.Adapter<RecyclerVIewAdapter.CategoryClassViewHolder> {
 
     List<CategoryClass> categoryClassAdapterList;
-    RecyclerVIewAdapter(List<CategoryClass> categoryClassAdapterList){
+
+    RecyclerVIewAdapter(List<CategoryClass> categoryClassAdapterList) {
         this.categoryClassAdapterList = categoryClassAdapterList;
     }
 
-    public static class CategoryClassViewHolder extends RecyclerView.ViewHolder{
+
+    public static class CategoryClassViewHolder extends RecyclerView.ViewHolder {
         CardView cv;
         TextView tv;
+        public LinearLayout Hold;
         public CategoryClassViewHolder(View itemView) {
             super(itemView);
-            cv = (CardView)itemView.findViewById(R.id.cv);
-            tv = (TextView)itemView.findViewById(R.id.tv);
+            cv = (CardView) itemView.findViewById(R.id.cv);
+            tv = (TextView) itemView.findViewById(R.id.tv);
+            Hold = (LinearLayout) itemView.findViewById(R.id.linearLayoutVar);
         }
     }
 
@@ -40,14 +47,21 @@ public class RecyclerVIewAdapter extends RecyclerView.Adapter<RecyclerVIewAdapte
     @Override
     public void onBindViewHolder(CategoryClassViewHolder holder, int position) {
         holder.tv.setText(categoryClassAdapterList.get(position).name);
+        holder.Hold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return categoryClassAdapterList.size();
     }
+
     @Override
-    public void onAttachedToRecyclerView(RecyclerView rcv){
+    public void onAttachedToRecyclerView(RecyclerView rcv) {
         super.onAttachedToRecyclerView(rcv);
     }
 
